f234674509efbcec0e780189e02589bf018bdd02
Brian6330 <games@schweigler.org>
2023-02-26 17:22:51 +0100

    IIM: Learning algorithm roughly implemented, started on imputation

--- IIM/main.py
+++ IIM/main.py
@@ -7,36 +7,62 @@ from sklearn.linear_model import LinearRegression, Ridge
 import time
 
 
-def recover_matrix_with_knn(matrix):
-    return knn_recovery(matrix, 5)
-
-
-def knn_recovery(matrix, k):
+def knn_recovery(matrix, matrix_nan, k):
     knn_euc = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+    learning(knn_euc, matrix, matrix_nan)
+    return matrix
+
+
+# Algorithm 1: Learning
+def learning(knn_euc, matrix, matrix_nan):
     model_params = []
-    incomplete_attributes = np.array([matrix.shape[0] + 1] * matrix.shape[1]).reshape(-1, 1)
-    for idx, tuple in enumerate(matrix, start=1):  # for t_i in r
-        # convert float to int (could be circumvented by multiplying)
-        tuple_int = tuple.astype(int).reshape(-1, 1)
-        idx_array = np.array([idx] * matrix.shape[1]).reshape(-1, 1)
-        knn_euc.fit(idx_array, tuple_int)
-        learning_neighbors = np.array(knn_euc.kneighbors()[1])  # k nearest neighbors
+    neighbors = []
+    matrix_scaled = (matrix * 1000).astype(int)  # convert float to int, minimizing rounding via multiplication
+    incomplete_tuples = np.array(np.where(np.isnan(matrix_nan)))
+    knn_euc.fit(np.arange(matrix.shape[0]).reshape(-1, 1), matrix_scaled)
+    for tuple in incomplete_tuples:  # for t_i in r
+        learning_neighbors = knn_euc.kneighbors(tuple.reshape(-1, 1))  # k nearest neighbors
+        neighbors.append(learning_neighbors[1])
 
         lr = Ridge()  # According to IIM paper, use Ridge regression
-        # Simply trying to predict the next tuple in the sequence, based on nearest neighbors
-        lr.fit(learning_neighbors, incomplete_attributes)
-        model_params.append(lr)  # alternatively: pass lr coefficients
+        # Fit linear regression based on neighbors of missing tuple
+        lr.fit(learning_neighbors[1], tuple)
+        model_params.append(lr)  # alternatively: pass lr coefficients?
+    k = 5
+    imputation(matrix, incomplete_tuples, k, model_params, matrix_nan, neighbors[1])
+
+
+# Algorithm 2: Imputation
+def imputation(matrix, incomplete_tuples, k, model_params, matrix_nan, imputation_neighbors):
+    matrix_scaled = (matrix * 1000).astype(int)
+    knn_euc = KNeighborsClassifier(n_neighbors=k, metric='euclidean')
+
+    knn_euc.fit(np.arange(matrix.shape[0]).reshape(-1, 1), matrix_scaled)
+    # should be for each missing tuple
+    for tuple_index in incomplete_tuples[0]:  # for t_i in r
+        imputation_neighbors = np.array(knn_euc.kneighbors()[1])  # k nearest neighbors
+
+        # impute missing values
+        tuple_with_missing_val = matrix_nan[tuple_index]
+        current_imputation_neighbors = imputation_neighbors[tuple_index]
+        for i in range(len(tuple_with_missing_val)):
+            if np.isnan(tuple_with_missing_val[i]):
+                # impute missing value
+                # TODO Fix exception thrown for 1 entry!
+                tuple_with_missing_val[i] = model_params[i].predict(current_imputation_neighbors.reshape(1, -1))
+
     return matrix
 
 
 def main(alg_code, filename_input, filename_output, runtime):
     # read input matrix
     matrix = np.loadtxt(filename_input, delimiter=' ')
+    matrix_with_nan = np.loadtxt("../Datasets/bafu/raw_matrices/BAFU_tiny_with_NaN.txt", delimiter=' ')
 
     # beginning of imputation process - start time measurement
     start_time = time.time()
 
-    matrix_imputed = recover_matrix_with_knn(matrix)
+    matrix_imputed = knn_recovery(matrix, matrix_with_nan, 5)
 
     # imputation is complete - stop time measurement
     end_time = time.time()
