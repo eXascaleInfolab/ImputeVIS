1fe4f20dac0160edcc9f300df120b9a6250fb5e9
brian6330 <bschweigler@fastforward.ch>
2023-06-18 10:38:53 +0200

    Wrapper: Add print statement for output

--- Zakhar/testWrapper.py
+++ Zakhar/testWrapper.py
@@ -4,4 +4,4 @@ mat = np.loadtxt("airq_normal.txt")
 mat_miss = np.copy(mat)
 mat_miss[50:149, 0] = np.nan
 mat_rec = algo_collection.native_cdrec(mat_miss, 2)
-np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100)
+print(np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100))
