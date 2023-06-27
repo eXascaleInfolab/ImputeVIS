b47f97413d00b9a13116fe2fcd26f237c72e12ca
brian6330 <bschweigler@fastforward.ch>
2023-06-27 18:00:44 +0200

    Wrapper: Add print statement for output

--- Zakhar/testWrapper.py
+++ Zakhar/testWrapper.py
@@ -6,10 +6,11 @@ mat_miss = np.copy(mat)
 mat_miss[50:149, 0] = np.nan
 
 mat_rec = algo_collection.native_cdrec_param(mat_miss, 2, 1E-6, 100)
-np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100)
+print(np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100))
 
 mat_rec = algo_collection.native_stmvl(mat_miss)
-np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100)
+print(np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100))
 
 mat_rec = algo_collection.native_stmvl_param(mat_miss, 7, 0.85, 2.0)
-np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100)
+print(np.sqrt(np.power(np.abs(mat_rec - mat), 2).sum() / 100))
+
