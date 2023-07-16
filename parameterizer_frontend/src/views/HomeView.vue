86e88a778409b777f327211ddcac5915183b7a42
Brian6330 <games@schweigler.org>
2023-07-16 12:16:20 +0200

    Comparison: Show errors per algo, improve performance with highchart-boost

--- parameterizer_frontend/src/views/HomeView.vue
+++ parameterizer_frontend/src/views/HomeView.vue
@@ -48,18 +48,48 @@
             </div>
           </div>
         </div>
+        <div class="col-lg-2 mt-5">
+          <div class="row">
+            <div class="col-xs">
+              <h6 v-if="rmseCDRec !== null && rmseCDRec !== ''"> CDRec RMSE: {{ rmseCDRec }}</h6>
+              <h6 v-if="maeCDRec !== null && maeCDRec !== ''"> CDRec MAE: {{ maeCDRec }}</h6>
+              <h6 v-if="miCDRec !== null && miCDRec !== ''"> CDRec MI: {{ miCDRec }}</h6>
+              <h6 v-if="corrCDRec !== null && corrCDRec !== ''"> CDRec CORR: {{ corrCDRec }}</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 mt-5">
+          <div class="row">
+            <div class="col-xs">
+              <h6 v-if="rmseIIM !== null && rmseIIM !== ''"> IIM RMSE: {{ rmseIIM }}</h6>
+              <h6 v-if="maeIIM !== null && maeIIM !== ''"> IIM MAE: {{ maeIIM }}</h6>
+              <h6 v-if="miIIM !== null && miIIM !== ''"> IIM MI: {{ miIIM }}</h6>
+              <h6 v-if="corrIIM !== null && corrIIM !== ''"> IIM CORR: {{ corrIIM }}</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 mt-5">
+          <div class="row">
+            <div class="col-xs">
+              <h6 v-if="rmseMRNN !== null && rmseMRNN !== ''"> M-RNN RMSE: {{ rmseMRNN }}</h6>
+              <h6 v-if="maeMRNN !== null && maeMRNN !== ''"> M-RNN MAE: {{ maeMRNN }}</h6>
+              <h6 v-if="miMRNN !== null && miMRNN !== ''"> M-RNN MI: {{ miMRNN }}</h6>
+              <h6 v-if="corrMRNN !== null && corrMRNN !== ''"> M-RNN CORR: {{ corrMRNN }}</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 mt-5">
+          <div class="row">
+            <div class="col-xs">
+              <h6 v-if="rmseSTMVL !== null && rmseSTMVL !== ''"> ST-MVL RMSE: {{ rmseSTMVL }}</h6>
+              <h6 v-if="maeSTMVL !== null && maeSTMVL !== ''"> ST-MVL MAE: {{ maeSTMVL }}</h6>
+              <h6 v-if="miSTMVL !== null && miSTMVL !== ''"> ST-MVL MI: {{ miSTMVL }}</h6>
+              <h6 v-if="corrSTMVL !== null && corrSTMVL !== ''"> ST-MVL CORR: {{ corrSTMVL }}</h6>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-4">
-          <h2 v-if="rmse !== null && rmse !== ''"> RMSE: {{ rmse }}</h2>
-          <h2 v-if="mae !== null && mae !== ''"> MAE: {{ mae }}</h2>
-        </div>
-        <div class="col-sm-4">
-          <h2 v-if="mi !== null && mi !== ''"> MI: {{ mi }}</h2>
-          <h2 v-if="corr !== null && corr !== ''"> CORR: {{ corr }}</h2>
-        </div>
-      </div>
       <highcharts v-if="imputedData" :options="chartOptionsImputed"></highcharts>
       <highcharts :options="chartOptionsOriginal"></highcharts>
     </div>
@@ -76,10 +106,12 @@ import {Chart} from 'highcharts-vue'
 import Highcharts from 'highcharts'
 import HC_exporting from 'highcharts/modules/exporting'
 import HC_exportData from 'highcharts/modules/export-data'
+import HighchartsBoost from "highcharts/modules/boost";
 
 // Initialize exporting modules
 HC_exporting(Highcharts)
 HC_exportData(Highcharts)
+HighchartsBoost(Highcharts)
 
 export default {
   components: {
@@ -90,15 +122,24 @@ export default {
     const dataSelect = ref('BAFU_quarter') // Default data is BAFU
     const fetchedData = reactive({});
     let loadingResults = ref(false);
+    
     //CDRec Parameters
     const missingRate = ref('1'); // Default missing rate is 1%
     const truncationRank = ref('1') // Default truncation rank is 1, 0 means detect truncation automatically
     const epsilon = ref('E-7'); // Default epsilon is E-7
     const iterations = ref(500); // Default number of iterations is 1000
+    const rmseCDRec = ref(null);
+    const maeCDRec = ref(null);
+    const miCDRec = ref(null);
+    const corrCDRec = ref(null);
 
     //IIM Parameters
     const numberSelect = ref(1); // Default selected learning neighbors is 1
     const typeSelect = ref(''); // Default selected type is "Normal", denoted by an empty string
+    const rmseIIM = ref(null);
+    const maeIIM = ref(null);
+    const miIIM = ref(null);
+    const corrIIM = ref(null);
 
     // M-RNN Parameters
     const learningRate = ref(0.01); // Default learning rate is 0.01
@@ -106,19 +147,23 @@ export default {
     const iterationsMRNN = ref(500); // Default number of iterations is 1000
     const keepProb = ref(0.5); // Default keep probability is 0.5
     const seqLen = ref(7); // Default sequence length is 7
+    const rmseMRNN = ref(null);
+    const maeMRNN = ref(null);
+    const miMRNN = ref(null);
+    const corrMRNN = ref(null);
 
     // ST-MVL Parameters
     const windowSize = ref('2'); // Default window size is 2
     const gamma = ref('0.5') // Default smoothing parameter gamma is 0.5, min 0.0, max 1.0
     const alpha = ref('2') // Default power for spatial weight (alpha) is 2, must be larger than 0.0
+    const rmseSTMVL = ref(null);
+    const maeSTMVL = ref(null);
+    const miSTMVL = ref(null);
+    const corrSTMVL = ref(null);
 
 
     const checkedNames = ref([]);
     const imputedData = ref(false); // Whether imputation has been carried out
-    const rmse = ref(null);
-    const mae = ref(null);
-    const mi = ref(null);
-    const corr = ref(null);
 
 
     const fetchData = async () => {
@@ -235,11 +280,34 @@ export default {
       }],
     });
 
+    function clearErrorMetrics() {
+      rmseCDRec.value = null;
+      maeCDRec.value = null;
+      miCDRec.value = null;
+      corrCDRec.value = null;
+
+      rmseIIM.value = null;
+      maeIIM.value = null;
+      miIIM.value = null;
+      corrIIM.value = null;
+
+      rmseMRNN.value = null;
+      maeMRNN.value = null;
+      miMRNN.value = null;
+      corrMRNN.value = null;
+
+      rmseSTMVL.value = null;
+      maeSTMVL.value = null;
+      miSTMVL.value = null;
+      corrSTMVL.value = null;
+    }
+
     const handleCheckboxChange = async () => {
       // Clear the existing series
       // chartOptionsImputed.value.series = [];
       loadingResults.value = true;
       chartOptionsImputed.value.series.splice(0, chartOptionsImputed.value.series.length)
+      clearErrorMetrics();
 
       for (let checkedName of checkedNames.value) {
         let dataSet = `${dataSelect.value}_obfuscated_${missingRate.value}`;
@@ -260,6 +328,10 @@ export default {
             );
             fetchedData[checkedName] = response.data;
           }
+          rmseCDRec.value = fetchedData[checkedName].rmse.toFixed(3);
+          maeCDRec.value = fetchedData[checkedName].mae.toFixed(3);
+          miCDRec.value = fetchedData[checkedName].mi.toFixed(3);
+          corrCDRec.value = fetchedData[checkedName].corr.toFixed(3);
           fetchedData[checkedName].matrix_imputed.forEach((data: number[], index: number) => {
             //The push should theoretically ensure that we are just adding
             chartOptionsImputed.value.series.push(createSeries(index, data, 'CDRec: Series'));
@@ -280,6 +352,10 @@ export default {
             );
             fetchedData[checkedName] = response.data;
           }
+          rmseIIM.value = fetchedData[checkedName].rmse.toFixed(3);
+          maeIIM.value = fetchedData[checkedName].mae.toFixed(3);
+          miIIM.value = fetchedData[checkedName].mi.toFixed(3);
+          corrIIM.value = fetchedData[checkedName].corr.toFixed(3);
           fetchedData[checkedName].matrix_imputed.forEach((data: number[], index: number) => {
             chartOptionsImputed.value.series.push(createSeries(index, data, 'IIM: Series'));
           });
@@ -290,7 +366,7 @@ export default {
                   data_set: dataSet,
                   hidden_dim: hiddenDim.value,
                   learning_rate: learningRate.value,
-                  iterations: iterations.value,
+                  iterations: iterationsMRNN.value,
                   keep_prob: keepProb.value,
                   seq_len: seqLen.value
                 },
@@ -302,6 +378,10 @@ export default {
             );
             fetchedData[checkedName] = response.data;
           }
+          rmseMRNN.value = fetchedData[checkedName].rmse.toFixed(3);
+          maeMRNN.value = fetchedData[checkedName].mae.toFixed(3);
+          miMRNN.value = fetchedData[checkedName].mi.toFixed(3);
+          corrMRNN.value = fetchedData[checkedName].corr.toFixed(3);
           fetchedData[checkedName].matrix_imputed.forEach((data: number[], index: number) => {
             chartOptionsImputed.value.series.push(createSeries(index, data, 'MRNN: Series'));
           });
@@ -322,6 +402,10 @@ export default {
             );
             fetchedData[checkedName] = response.data;
           }
+          rmseSTMVL.value = fetchedData[checkedName].rmse.toFixed(3);
+          maeSTMVL.value = fetchedData[checkedName].mae.toFixed(3);
+          miSTMVL.value = fetchedData[checkedName].mi.toFixed(3);
+          corrSTMVL.value = fetchedData[checkedName].corr.toFixed(3);
           fetchedData[checkedName].matrix_imputed.forEach((data: number[], index: number) => {
             chartOptionsImputed.value.series.push(createSeries(index, data, 'ST-MVL: Series'));
           });
@@ -359,16 +443,27 @@ export default {
 
     return {
       submitForm,
-      rmse,
-      mae,
-      mi,
-      corr,
+      // Error Metrics
+      rmseCDRec,
+      maeCDRec,
+      miCDRec,
+      corrCDRec,
+      rmseIIM,
+      maeIIM,
+      miIIM,
+      corrIIM,
+      rmseMRNN,
+      maeMRNN,
+      miMRNN,
+      corrMRNN,
+      rmseSTMVL,
+      maeSTMVL,
+      miSTMVL,
+      corrSTMVL,
+      // End Error Metrics
       chartOptionsOriginal,
       chartOptionsImputed,
       dataSelect,
-      truncationRank,
-      epsilon,
-      iterations,
       missingRate,
       imputedData,
       checkedNames,
