9f529eb206fec95e26dac2d693be8e7fdfca3864
Brian6330 <games@schweigler.org>
2023-08-04 21:24:06 +0200

    Optimization: Successive halving all datasets

--- Optimizer/successive_halving.py
+++ Optimizer/successive_halving.py
@@ -142,9 +142,9 @@ if __name__ == '__main__':
     # ))
     algos = ['stmvl']
     # todo handle drift separately
-    datasets = ['bafu', 'chlorine', 'climate', 'meteo']
-    dataset_files = ['BAFU', 'cl2fullLarge', 'climate', 'meteo_total']
-    metrics = ['rmse', 'mae']
+    datasets = ['bafu', 'chlorine', 'climate', 'drift', 'meteo']
+    dataset_files = ['BAFU', 'cl2fullLarge', 'climate', 'batch10', 'meteo_total']
+    metrics = ['mae']
 
     results = {}
     for algo in algos:
@@ -152,6 +152,9 @@ if __name__ == '__main__':
             raw_file_path = f"../Datasets/{dataset}/raw_matrices/{data_file}_eighth.txt"
             obf_file_path = f"../Datasets/{dataset}/obfuscated/{data_file}_eighth_obfuscated_10.txt"
 
+            if dataset == 'drift':
+                raw_file_path = f"../Datasets/{dataset}/drift10/raw_matrices/{data_file}_eighth.txt"
+                obf_file_path = f"../Datasets/{dataset}/obfuscated/{data_file}_eighth_obfuscated_10.txt"
             raw_matrix = np.loadtxt(raw_file_path, delimiter=" ", )
             obf_matrix = np.loadtxt(obf_file_path, delimiter=" ", )
 
@@ -175,7 +178,7 @@ if __name__ == '__main__':
             }
 
         # Save results in a JSON file
-        with open(f'optimization_results_{algo}_succesive_halving_{metrics[0] + "_" + metrics[1]}.json',
+        with open(f'optimization_results_{algo}_succesive_halving_{metrics[0]}.json',
                   'w') as outfile:
             json.dump(results, outfile)
 
