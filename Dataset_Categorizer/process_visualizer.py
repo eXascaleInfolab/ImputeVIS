9f0da9a50b6e7a073ac490e2a72933bf2a81d4e9
Brian6330 <games@schweigler.org>
2023-08-30 15:07:36 +0200

    Presentation: Equiprobable binning

--- Dataset_Categorizer/process_visualizer.py
+++ Dataset_Categorizer/process_visualizer.py
@@ -30,11 +30,6 @@ def detrend_spline_and_peak(y, knot_position=0.5, plot=False):
     -----
     This function uses `statsmodels` for OLS regression and ACF computation. The time variable
     is automatically generated based on the length of `y`.
-
-    Examples
-    --------
-    >>> y = np.sin(np.linspace(0, 10, 100)) + np.random.normal(0, 0.1, 100)
-    >>> detrended_y, first_peak = detrend_spline_and_peak(y)
     """
 
     # Generate time variable
@@ -59,13 +54,13 @@ def detrend_spline_and_peak(y, knot_position=0.5, plot=False):
 
     # Detrend the series
     detrended_y = y - predicted_trend
-    detrended_y_to_plot = detrended_y.copy()[:,1]
-    y_to_plot = y.copy()[:,1]
+    detrended_y_to_plot = detrended_y.copy()[:, 1]
+    y_to_plot = y.copy()[:, 1]
 
     if plot:
         plt.figure(figsize=(10, 6))
         plt.plot(time, y_to_plot, label='Original Series')
-        #plt.plot(time, predicted_trend, label='Predicted Trend', linestyle='--')
+        # plt.plot(time, predicted_trend, label='Predicted Trend', linestyle='--')
         plt.plot(time, detrended_y_to_plot, label='Detrended Series')
         plt.axvline(x=knot, color='red', linestyle=':', label='Knot')
         plt.legend()
@@ -86,21 +81,20 @@ def detrend_spline_and_peak(y, knot_position=0.5, plot=False):
 
         plt.figure(figsize=(10, 6))
         plt.stem(acf_values, use_line_collection=True, basefmt=" ", linefmt="-b", markerfmt="ob")
-        #plt.set_title('Auto-correlation Function')
+        # plt.set_title('Auto-correlation Function')
         if first_peak is not None:
             plt.plot(first_peak, acf_values[first_peak], 'or', markersize=10, label='First Peak')
             plt.legend()
 
-
         # Plotting the series and trend
-        #fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+        # fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
         # Original, trend, detrended
-#         axs[0].plot(y_to_plot, label='Original Series')
-# #        axs[0].plot(detrended_y_to_plot + predicted_trend, label='Predicted Trend', linestyle='--')
-#         axs[0].plot(detrended_y_to_plot, label='Detrended Series')
-#         axs[0].axvline(x=knot, color='red', linestyle=':', label='Knot')
-#         axs[0].legend()
+        #         axs[0].plot(y_to_plot, label='Original Series')
+        # #        axs[0].plot(detrended_y_to_plot + predicted_trend, label='Predicted Trend', linestyle='--')
+        #         axs[0].plot(detrended_y_to_plot, label='Detrended Series')
+        #         axs[0].axvline(x=knot, color='red', linestyle=':', label='Knot')
+        #         axs[0].legend()
 
         # ACF
         # axs[1].stem(acf_values, use_line_collection=True, basefmt=" ", linefmt="-b", markerfmt="ob")
@@ -117,14 +111,155 @@ def detrend_spline_and_peak(y, knot_position=0.5, plot=False):
     return detrended_y, first_peak
 
 
+def equiprobable_binning(ts: np.array) -> np.array:
+    """
+    Divides the time series into three equiprobable bins: A, B, C.
+
+    Parameters
+    ----------
+    ts : np.array
+        The time series to be binned.
+
+    Returns
+    -------
+    np.array
+        An array of bin labels ('A', 'B', or 'C') for each value in the time series.
+    """
+    sorted_indices = np.argsort(ts)
+    n = len(ts)
+    bin_size = n // 3
+
+    bin_labels = np.empty_like(ts, dtype=np.chararray)
+    bin_labels[sorted_indices[:bin_size]] = 'A'
+    bin_labels[sorted_indices[bin_size:2 * bin_size]] = 'B'
+    bin_labels[sorted_indices[2 * bin_size:]] = 'C'
+
+    return bin_labels
+
+
+def two_symbol_sequences(bin_labels: np.array) -> list:
+    """
+    Creates a list of two-symbol sequences from the binned labels.
+
+    Parameters
+    ----------
+    bin_labels : np.array
+        An array of bin labels from equiprobable binning.
+
+    Returns
+    -------
+    list
+        List of two-symbol sequences.
+    """
+    return [bin_labels[i] + bin_labels[i + 1] for i in range(len(bin_labels) - 1)]
+
+
+def calculate_entropy(sequences: list) -> float:
+    """
+    Calculates the entropy of two-symbol sequences.
+
+    Parameters
+    ----------
+    sequences : list
+        List of two-symbol sequences.
+
+    Returns
+    -------
+    float
+        Entropy of the sequences.
+    """
+    freq = {}
+    for seq in sequences:
+        if seq in freq:
+            freq[seq] += 1
+        else:
+            freq[seq] = 1
+
+    entropy = 0
+    total = len(sequences)
+    for key in freq:
+        prob = freq[key] / total
+        entropy -= prob * np.log2(prob)
+
+    return entropy
+
+
+def visualize_binning(ts: np.array, bin_labels: np.array) -> None:
+    """
+    Visualizes the equiprobable binning using horizontal lines and labels.
+
+    Parameters
+    ----------
+    ts : np.array
+        The time series data.
+    bin_labels : np.array
+        An array of bin labels for the time series.
+
+    Returns
+    -------
+    None
+    """
+    fig, ax = plt.subplots()
+
+    ax.plot(ts, '-o', label='Time Series', alpha=0.7)
+
+    # Get unique values of the time series
+    unique_values = np.unique(ts)
+
+    # Get the threshold values for bin boundaries
+    lower_threshold = unique_values[len(unique_values) // 3]
+    upper_threshold = unique_values[2 * len(unique_values) // 3]
+
+    ax.axhline(y=lower_threshold, color='r', linestyle='--')
+    ax.axhline(y=upper_threshold, color='r', linestyle='--')
+
+    # Position the labels
+    label_y_position = (min(ts) + lower_threshold) / 2
+    ax.text(0, label_y_position, 'A', color='red', verticalalignment='center', horizontalalignment='left', fontsize=12,
+            fontweight='bold')
+
+    label_y_position = (lower_threshold + upper_threshold) / 2
+    ax.text(0, label_y_position, 'B', color='red', verticalalignment='center', horizontalalignment='left', fontsize=12,
+            fontweight='bold')
+
+    label_y_position = (upper_threshold + max(ts)) / 2
+    ax.text(0, label_y_position, 'C', color='red', verticalalignment='center', horizontalalignment='left', fontsize=12,
+            fontweight='bold')
+
+    ax.set_title("Equiprobable Binning")
+    ax.set_xlabel("Index")
+    ax.set_ylabel("Value")
+    plt.grid(True)
+
+    # Save the plot
+    plt.savefig('results/equiprobable_binning_plot.png')
+    plt.show()
+
+
 def main():
+    ### Wang's periodicity Metric
     # Sample usage
     np.random.seed(42)
-    y_sample = np.sin(np.linspace(0, 10, 100)) + np.random.normal(0, 0.1, 100)
+    # y_sample = np.sin(np.linspace(0, 10, 100)) + np.random.normal(0, 0.1, 100)
     y_sample = np.loadtxt("../Datasets/meteo/raw_matrices/meteo_normal_full_eighth.txt", delimiter=" ", )
-    detrended_y_sample, first_peak_sample = detrend_spline_and_peak(y_sample[4000:6000,], plot=True)
+    detrended_y_sample, first_peak_sample = detrend_spline_and_peak(y_sample[4000:6000, ], plot=True)
 
     print(f"First ACF Peak at lag: {first_peak_sample}")
+
+    ### Equiprobable-Binning
+    # ts = np.array([1, 5, 3, 8, 7, 2, 4, 6])
+    ts = y_sample.copy()[:, 1]
+    ts = ts[4000:6000]
+    bin_labels = equiprobable_binning(ts)
+    sequences = two_symbol_sequences(bin_labels)
+    entropy = calculate_entropy(sequences)
+
+    print(f"Binned labels: {bin_labels}")
+    print(f"Two-symbol sequences: {sequences}")
+    print(f"Entropy: {entropy}")
+
+    visualize_binning(ts, bin_labels)
+
 
 if __name__ == "__main__":
     main()
