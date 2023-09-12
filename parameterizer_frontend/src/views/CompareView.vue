057e661a110425abb762f732b6851eaa34e244db
Brian6330 <games@schweigler.org>
2023-09-12 15:50:36 +0200

    Frontend: Fix normalization bug on compare

--- parameterizer_frontend/src/views/CompareView.vue
+++ parameterizer_frontend/src/views/CompareView.vue
@@ -583,6 +583,7 @@ export default {
     }
       const handleNormalizationModeChange = () => {
       if (imputedData.value == true) {
+          fetchData();
           submitForm();
       } else {
           handleDataSelectChange();
